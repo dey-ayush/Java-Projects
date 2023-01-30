39fbc0e34255baee20faf122cd078c5ff88a60aa
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2023-01-30 20:51:13 +0530

    Digital Clock

---
+++ Creating Digital Clock/src/com/practice/Main.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing...");
+		MyWindow w= new MyWindow();
+
+	}
+
+}
