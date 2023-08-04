2493d09dd984ff64335a0312e24dabee795d238b
Ayush Dey <62686696+dey-ayush@users.noreply.github.com>
2023-08-04 19:42:40 +0530

    Student Management App
    Simple Student Management Login and Register Page using xampp sql backend support

---
+++ SimpleStudentM/src/StudentManagement/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StudentManagement;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author ayush
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhoststudentmanagement, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
